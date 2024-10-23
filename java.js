d0abfe38b1c8c83da4bc50c6fe2668d848deb662
Leonardo <leonardocfernanndes@gmail.com>
2024-10-23 12:03:43 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,5 @@
+// Selecione o elemento com o ID "minha-ancora"
+const minhaAncora = document.getElementById('minha-ancora');
+
+// Redirecione o usuário para a âncora
+minhaAncora.scrollIntoView();
